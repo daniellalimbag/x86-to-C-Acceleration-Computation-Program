4fd8c6e3a0425279a050ee6334251b782cbeaf6d
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2024-11-27 05:39:34 +0800

    refactor: deleted initialized values in memory

--- AccelerationComputation.asm
+++ AccelerationComputation.asm
@@ -6,12 +6,6 @@
 
 %include 'io64.inc'
 section .data
-    ;input matrix w the dataaa (V0, V1, Time)
-    matrix dq 0.0, 62.5, 10.1
-           dq 60.0, 122.3, 5.5
-           dq 30.0, 160.7, 7.8
-
-    matrixRows dq 3 ;num of rows of matrix
     kmToM dq 1000.0
     hoursToSec dq 3600.0
     
