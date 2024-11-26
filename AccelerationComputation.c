042b6c577225dde626c2c37010552b5ee9b90932
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2024-11-27 05:31:31 +0800

    feat: declared stuff in c

--- AccelerationComputation.c
+++ AccelerationComputation.c
@@ -6,7 +6,15 @@ Daniella Limbag
 Ma. Julianna Reyes
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+extern void x64CompAcceleration(long long int rows, float* matrix, int* results);
 
 int main() {
-	    return 0;
+	int rows;
+    clock_t start, end;
+    double time_used, average_time;
+    int loopFlag;
+	return 0;
 }
